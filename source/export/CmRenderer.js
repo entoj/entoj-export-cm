8491cde0526d606beb2c611eeebeae9e98005cc5
Christian Auth <c.auth@syzygy.de>
2017-09-12 19:09:15 +0200

    feat(export.CmRenderer): adds settingsService type declartion

--- source/export/CmRenderer.js
+++ source/export/CmRenderer.js
@@ -29,6 +29,7 @@ class CmRenderer extends JspRenderer
             {
                 source+= '<%@ taglib prefix="cm" uri="http://www.coremedia.com/2004/objectserver-1.0-2.0"%>';
                 source+= '<%@ taglib prefix="bp" uri="http://www.coremedia.com/2012/blueprint"%>';
+                source+= '<!-- <%--@elvariable id="settingsService" type="com.coremedia.blueprint.base.settings.SettingsService"--%> -->';
                 return source;
             });
         return promise;
