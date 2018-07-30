d2a54bdd938a300c8793ee3c0c411bb095af6485
Artur Baron <P224435@tk.de>
2018-07-30 15:00:28 +0200

    removed html comments and adjusted the test cases

--- source/export/CmRenderer.js
+++ source/export/CmRenderer.js
@@ -31,11 +31,11 @@ class CmRenderer extends JspRenderer
                 {
                     source+= '<%@ taglib prefix="cm" uri="http://www.coremedia.com/2004/objectserver-1.0-2.0"%>';
                     source+= '<%@ taglib prefix="bp" uri="http://www.coremedia.com/2012/blueprint"%>';
-                    source+= '<!-- <%--@elvariable id="settingsService" type="com.coremedia.blueprint.base.settings.SettingsService"--%> -->';
+                    source+= '<%--@elvariable id="settingsService" type="com.coremedia.blueprint.base.settings.SettingsService"--%>';
                     if (macroConfiguration && macroConfiguration.namespace && macroConfiguration.type)
                     {
-                        source+='<!-- <%--@elvariable id="self" type="' + macroConfiguration.namespace + '.' + macroConfiguration.type + '"--%> -->';
-                        source+='<!-- <%--@elvariable id="model" type="' + macroConfiguration.namespace + '.' + macroConfiguration.type + '"--%> -->';
+                        source+='<%--@elvariable id="self" type="' + macroConfiguration.namespace + '.' + macroConfiguration.type + '"--%>';
+                        source+='<%--@elvariable id="model" type="' + macroConfiguration.namespace + '.' + macroConfiguration.type + '"--%>';
                     }
                     return source;
                 });
